f3c378c5729e4a2afe9a70d02462d25cc639cca1
Jeden1102 <bum123@o2.pl>
2022-06-19 18:18:22 +0200

    init

---
+++ dist/precache-manifest.23bc6eb4274decf2d40d8dbfe09fac2f.js
@@ -0,0 +1,126 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ff71486df0313bdab6265241d50a32fc",
+    "url": "/Madziulka/assets/covers/huggingyou.jpg"
+  },
+  {
+    "revision": "2acb8d38a4881edfd4104b024f291498",
+    "url": "/Madziulka/assets/covers/stacja.jpg"
+  },
+  {
+    "revision": "3afd2fa0c7c238527850116b8f9a9766",
+    "url": "/Madziulka/assets/covers/untill.jpg"
+  },
+  {
+    "revision": "c9858cb71cac49fabd6b904ed9dde5f1",
+    "url": "/Madziulka/assets/forward-button.png"
+  },
+  {
+    "revision": "5a202b9e1837b8656a4fc1ed42f00e41",
+    "url": "/Madziulka/assets/heart.png"
+  },
+  {
+    "revision": "9557a002a16815b9307966679728e57c",
+    "url": "/Madziulka/assets/jarmark.webp"
+  },
+  {
+    "revision": "82b9c7a5a3f405032b1db71a25f67021",
+    "url": "/Madziulka/assets/logo.png"
+  },
+  {
+    "revision": "63a7d78d42c33b94fc7b957524795cac",
+    "url": "/Madziulka/assets/logo.svg"
+  },
+  {
+    "revision": "0eab0402d1281f9a0d1ea8aeba639b5d",
+    "url": "/Madziulka/assets/next1.png"
+  },
+  {
+    "revision": "c00e48b4937107ac6e509f7e59a947ad",
+    "url": "/Madziulka/assets/pause.png"
+  },
+  {
+    "revision": "8f33c5ac359ed93a81ead7245ab06483",
+    "url": "/Madziulka/assets/play.png"
+  },
+  {
+    "revision": "c1cbdd9b5ac23e347be4e45671f18526",
+    "url": "/Madziulka/assets/play1.png"
+  },
+  {
+    "revision": "1b9ab2908fae1fa1deb756b3e4fb9aa6",
+    "url": "/Madziulka/assets/previous.png"
+  },
+  {
+    "revision": "051a313cc8bda4dbaeab7de4ecd4b604",
+    "url": "/Madziulka/assets/previous1.png"
+  },
+  {
+    "revision": "3d45e5aadefbfecc59df7196cf22a618",
+    "url": "/Madziulka/assets/songs/huggingyou.mp3"
+  },
+  {
+    "revision": "97d0fbefb9d67f01387e5b26a897d5de",
+    "url": "/Madziulka/assets/songs/stacja.mp3"
+  },
+  {
+    "revision": "36882b718b9b59f2995e12a48e23b6fe",
+    "url": "/Madziulka/assets/songs/untill.mp3"
+  },
+  {
+    "revision": "e3acbb3ace03ec48fb9c668fa8946c2c",
+    "url": "/Madziulka/assets/stop.png"
+  },
+  {
+    "revision": "9acd34157f4fab8d4db3ea978ea58f27",
+    "url": "/Madziulka/assets/tailwind.css"
+  },
+  {
+    "revision": "ceaa01335409daa424c2",
+    "url": "/Madziulka/css/app.59d3e8bd.css"
+  },
+  {
+    "revision": "a9ad6851900345d1d818",
+    "url": "/Madziulka/css/chunk-vendors.17f63ec9.css"
+  },
+  {
+    "revision": "5a202b9e1837b8656a4fc1ed42f00e41",
+    "url": "/Madziulka/img/heart.5a202b9e.png"
+  },
+  {
+    "revision": "0eab0402d1281f9a0d1ea8aeba639b5d",
+    "url": "/Madziulka/img/next1.0eab0402.png"
+  },
+  {
+    "revision": "8f33c5ac359ed93a81ead7245ab06483",
+    "url": "/Madziulka/img/play.8f33c5ac.png"
+  },
+  {
+    "revision": "051a313cc8bda4dbaeab7de4ecd4b604",
+    "url": "/Madziulka/img/previous1.051a313c.png"
+  },
+  {
+    "revision": "e3acbb3ace03ec48fb9c668fa8946c2c",
+    "url": "/Madziulka/img/stop.e3acbb3a.png"
+  },
+  {
+    "revision": "a462ecbc58d3bf67beda9aeba3ac2f83",
+    "url": "/Madziulka/index.html"
+  },
+  {
+    "revision": "ceaa01335409daa424c2",
+    "url": "/Madziulka/js/app.df4473c3.js"
+  },
+  {
+    "revision": "a9ad6851900345d1d818",
+    "url": "/Madziulka/js/chunk-vendors.c4dd1b91.js"
+  },
+  {
+    "revision": "cac439e5627fd04e389de3e7dd18fecb",
+    "url": "/Madziulka/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/Madziulka/robots.txt"
+  }
+]);
